12def1fbe93c93a902a0b660434f7f02c9e8df51
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-14 00:36:49 +0530

    Testing Email feature

--- user_end/views.py
+++ user_end/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
+from django.http import HttpResponse
 # Create your views here.
+def user_view(request):
+    
+    send_mail('subject', 'body of the message', 'noreply@bottlenose.co', ['vishwanathnaryanan29@gmail.com'])
+    return HttpResponse("Mail Sent")
