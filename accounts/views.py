e306f5b1addd6b9bbf6f3f53803f0f5424f4760d
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-14 14:50:06 +0530

    Created Custom User Model with Email as PK

--- accounts/views.py
+++ accounts/views.py
@@ -1,47 +1,50 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth .models import User,auth
+from django.contrib.auth .models import auth
 from django.contrib import messages
+from .models import user
 
 
 def home(request):
     return render(request,'accounts/home.html')
 
 def login(request):
+    print('hello')
     if(request.method=="POST"):
-        user_name=request.POST['username']
+        email=request.POST['emailid']
         passwd=request.POST['password']
-        user=auth.authenticate(request,username=user_name,password=passwd)
+        print("Email : ",email)
+        user=auth.authenticate(request,email=email,password=passwd)
         if(user is not None):
             auth.login(request,user)
+            print("Success")
             print("user login successfully")
             return redirect("/")
         else:
+            print("Failed")
             messages.error(request,"Invalid Credentials")
             return redirect("login")
     else:
         return render(request,'accounts/login.html')
 
 def register(request):
-    # if(request.method=="POST"):
-    #     name=request.POST['name1']
-    #     email=request.POST['email_ID']
-    #     street_name=request.POST['street_name']
-    #     area=request.POST['area_name']
-    #     city=request.POST['city']
-    #     state=request.POST['state']
-    #     pincode=request.POST['pincode']
-    #     passwd=request.POST['password1']
-    #     confirm_passwd=request.POST['password2']
+    if(request.method=="POST"):
+        name=request.POST['name1']
+        email=request.POST['email_ID']
+        passwd=request.POST['password1']
+        confirm_passwd=request.POST['password2']
 
-    #     if(passwd==confirm_passwd):
-    #         if User.objects.filter(username=name).exists():
-    #             messages.error(request,"Username already Exists")
-    #             return redirect("register")
-    #         elif User.objects.filter(email=email).exists():
-    #             messages.error(request,"Already Registered with this mail ID")
-    #             return redirect("register")
-    #     else:
-    #         messages.error(request,"Password Not Matching")
-    #         return redirect("register")
+        if(passwd==confirm_passwd):
+            if user.objects.filter(username=name).exists():
+                messages.error(request,"Username already Exists")
+                return redirect("register")
+            elif user.objects.filter(email=email).exists():
+                messages.error(request,"Already Registered with this mail ID")
+                return redirect("register")
+            else :
+                 userobj=user.objects.create_user(username=name,email=email,password=passwd)
+                 userobj.save()
+        else:
+            messages.error(request,"Password Not Matching")
+            return redirect("register")
 
     return render(request,'accounts/register.html')
