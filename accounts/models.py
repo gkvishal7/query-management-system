ef3c31c91095b5b47ead252e44f04167671b9c5d
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-14 18:31:29 +0530

    Fixed Bugs

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class userManager(BaseUserManager):   
     def create_user(self,username, email, password, **extra_fields):  
         if not email:  
-            raise ValueError(_('The Email must be set'))  
+            raise ValueError(('The Email must be set'))  
         email = self.normalize_email(email)  
         user = self.model(username=username,email=email, **extra_fields)  
         user.set_password(password)  
