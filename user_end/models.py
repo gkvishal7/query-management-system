b4a102e81bd12ba754be26acc5038213b5344b15
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-15 00:34:05 +0530

    Added Title and Description

--- user_end/models.py
+++ user_end/models.py
@@ -3,7 +3,8 @@ from django.conf import settings
 
 #query model for storing query 
 class query(models.Model):
-    query_info = models.CharField(max_length=1000)
+    query_title = models.CharField(max_length=100)
+    query_description = models.CharField(max_length=1000)
     user_details = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     query_date= models.DateField()
     query_time=models.TimeField()
