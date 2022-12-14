1a97347bf4c17afd2de8634766d4b72df71d59f0
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-14 21:58:01 +0530

    Created query model for storing query in DB

--- user_end/models.py
+++ user_end/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+#query model for storing query 
+class query(models.Model):
+    query_info = models.CharField(max_length=1000)
+    user_details = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    query_date= models.DateField()
+    query_time=models.TimeField()
