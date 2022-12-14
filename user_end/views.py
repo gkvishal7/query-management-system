b826594a4df9aaaa5511ed59404e2b73cee3b690
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-15 00:34:33 +0530

    Added Title and Description

--- user_end/views.py
+++ user_end/views.py
@@ -16,11 +16,13 @@ def user_view(request):
     if request.method=="POST":
         #Getting data from POST request
         query_date=request.POST['date']
-        query_data=request.POST['query']
+        query_date = datetime.strptime(query_date, '%m/%d/%Y').date()
+        query_title=request.POST['query_title']
+        query_data=request.POST['query_description']
         query_time=datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%H:%M:%S")
         
         #Creating query model object for storing query in DB
-        query_object=query(query_info=query_data,query_date=query_date,query_time=query_time,user_details=request.user)
+        query_object=query(query_title=query_title,query_description=query_data,query_date=query_date,query_time=query_time,user_details=request.user)
         query_object.save()
     #send_mail('subject', 'body of the message', 'noreply@bottlenose.co', ['vishwanathnaryanan29@gmail.com'])
     return render(request,"user_end/user_end.html")
