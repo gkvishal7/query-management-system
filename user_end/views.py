f6a1c7f02c68e56218aaee492564412efe9814ab
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-14 21:58:39 +0530

    Created Method to get data from query form and store in DB using query model

--- user_end/views.py
+++ user_end/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+#importing custom user model and query model
+from accounts.models import user
+from .models import query
+#pytz for handling time-zone
+import pytz
+#for getting current time
+from datetime import datetime
+
+#Only Logged in User can access the view
+@login_required(login_url='/login/')
 def user_view(request):
-    
+    if request.method=="POST":
+        #Getting data from POST request
+        query_date=request.POST['date']
+        query_data=request.POST['query']
+        query_time=datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%H:%M:%S")
+        
+        #Creating query model object for storing query in DB
+        query_object=query(query_info=query_data,query_date=query_date,query_time=query_time,user_details=request.user)
+        query_object.save()
     #send_mail('subject', 'body of the message', 'noreply@bottlenose.co', ['vishwanathnaryanan29@gmail.com'])
     return render(request,"user_end/user_end.html")
