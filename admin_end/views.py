5cef49db816ad3aedc7cff240a4bcf4a4874c228
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-16 00:46:26 +0530

    Added Approving Functionality

--- admin_end/views.py
+++ admin_end/views.py
@@ -6,11 +6,16 @@ from accounts.models import user
 def admin(request):
     if request.method=="GET":
         query_list=list(query.objects.filter(query_status=False).values())
-        
         for i in query_list:
             i['user_details_id']=user.getusername(user.objects.get(id=i.get("user_details_id"))).capitalize()
-
         return render(request,"admin_end/admin_end.html",{'query_list':query_list})
     if request.method=="POST":
+        queryid=(request.POST['queryid'])
+        querytochange=query.objects.get(id=queryid)
+        querytochange.query_status=True
+        querytochange.save()
+        query_list=list(query.objects.filter(query_status=False).values())
+        for i in query_list:
+            i['user_details_id']=user.getusername(user.objects.get(id=i.get("user_details_id"))).capitalize()
+        return render(request,"admin_end/admin_end.html",{'query_list':query_list})
         
-        return render(request,"admin_end/admin_end.html")
