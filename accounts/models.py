c2bb0bd443ae0648a3b9bbad7359ed6e04c780dc
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-15 13:34:41 +0530

    Created Custom user Model

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin  
 from django.contrib.auth.base_user import BaseUserManager  
 from django.utils import timezone
-from django.contrib.auth.base_user import BaseUserManager  
 from django.utils.translation import gettext as _
 
 #Custom Model Manager 
@@ -20,11 +19,10 @@ class userManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)  
         extra_fields.setdefault('is_superuser', True)  
         extra_fields.setdefault('is_active', True)  
-        return self.create_user(email,username,password,**extra_fields)
-
+        return self.create_user(email=email,username=username,password=password,**extra_fields)
       
 #Custom User Model
-class user(AbstractBaseUser, PermissionsMixin,):  
+class user(AbstractBaseUser, PermissionsMixin):  
     username = models.CharField(max_length=45)
     email = models.EmailField(unique=True, max_length = 200)  
     date_joined = models.DateTimeField(default=timezone.now)  
@@ -32,9 +30,10 @@ class user(AbstractBaseUser, PermissionsMixin,):
     is_active = models.BooleanField(default=True)  
     objects = userManager() 
     USERNAME_FIELD = 'email'  
+    EMAIL_FIELD='email'
     REQUIRED_FIELDS = ['username']  
     def has_perm(self, perm, obj=None):  
         return True   
     def __str__(self):  
         return self.email
-     
+
