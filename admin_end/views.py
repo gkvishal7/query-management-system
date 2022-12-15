229e436c3bbe85d905ee17246d25ab1ab47e401b
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-16 00:10:41 +0530

    Fetch Values from Query DB and Pass to template

--- admin_end/views.py
+++ admin_end/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
-
+from user_end.models import query
+from accounts.models import user
 @login_required(login_url='/login/')
 def admin(request):
-    return render(request,"admin_end/admin.html")
+    if request.method=="GET":
+        query_list=list(query.objects.filter(query_status=False).values())
+        
+        for i in query_list:
+            i['user_details_id']=user.getusername(user.objects.get(id=i.get("user_details_id"))).capitalize()
+
+        return render(request,"admin_end/admin_end.html",{'query_list':query_list})
+    if request.method=="POST":
+        
+        return render(request,"admin_end/admin_end.html")
