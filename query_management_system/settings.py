2c763f28a557427f27f04015b0bc480c6a3ee591
gkvishal7 <gkvishal7@gmail.com>
2022-12-16 00:35:04 +0530

    admin UI done

--- query_management_system/settings.py
+++ query_management_system/settings.py
@@ -77,25 +77,26 @@ WSGI_APPLICATION = 'query_management_system.wsgi.application'
 
 # Database
 #https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'query-management-system',
-        'USER':'vishwa',
-        'PASSWORD':'1234',
-        'HOST':'localhost'
-    }
-}
 
-# DATABASES={
-#     'default':{
+# DATABASES = {
+#     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'query-management-system',
-#         'USER':'postgres',
-#         'PASSWORD':'001',
+#         'USER':'vishwa',
+#         'PASSWORD':'1234',
 #         'HOST':'localhost'
 #     }
 # }
+
+DATABASES={
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'query-management-system',
+        'USER':'postgres',
+        'PASSWORD':'001',
+        'HOST':'localhost'
+    }
+}
 
 # DATABASES = {
 #     'default': {
