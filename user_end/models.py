19e8bd218271e90f560bfb1edd2d9919b1f9db2d
gkvishal7 <gkvishal7@gmail.com>
2022-12-15 16:41:35 +0530

    queryTable issue resolved

--- user_end/models.py
+++ user_end/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 #query model for storing query 
 class query(models.Model):
-    query_title = models.CharField(max_length=100)
+    query_title = models.CharField(max_length=100,default="")
     query_description = models.CharField(max_length=1000)
     user_details = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     query_date= models.DateField()
