8f2e8d8320069e541269a88a0ac358e8aa53e775
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-16 14:35:44 +0530

    Added logout and history for user

--- user_end/urls.py
+++ user_end/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('querysubmit/', views.user_view, name='querysubmit'),
+    path('logout/',views.logout_user,name=''),
+    path('querysubmit/user_history/',views.history,name=''),
+
 ]
--- user_end/views.py
+++ user_end/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 #importing custom user model and query model
 from accounts.models import user
 from .models import query
@@ -24,5 +26,15 @@ def user_view(request):
         #Creating query model object for storing query in DB
         query_object=query(query_title=query_title,query_description=query_data,query_date=query_date,query_time=query_time,user_details=request.user)
         query_object.save()
-    #send_mail('subject', 'body of the message', 'noreply@bottlenose.co', ['vishwanathnaryanan29@gmail.com'])
+    
     return render(request,"user_end/user_end.html")
+def logout_user(request):
+    logout(request)
+    return redirect("/")
+def history(request):
+    user_details=request.user
+    print(user_details)
+    query_list=list(query.objects.filter(user_details_id=request.user).values())
+    for i in query_list:
+            i['user_details_id']=user.getusername(user.objects.get(id=i.get("user_details_id"))).capitalize()
+    return render(request,"user_end/user_end_history.html",{'query_list':query_list})
