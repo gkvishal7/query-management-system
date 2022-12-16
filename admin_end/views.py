8838b0a1e1c6ca865a01cb813ba442ecb3182b4c
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-16 21:40:32 +0530

    Separated Views and Acknowledgement

--- admin_end/views.py
+++ admin_end/views.py
@@ -13,21 +13,24 @@ def admin(request):
             i['user_details_id']=user.getusername(user.objects.get(id=i.get("user_details_id"))).capitalize()
         return render(request,"admin_end/admin_end.html",{'query_list':query_list})
     if request.method=="POST":
-        queryid=(request.POST['queryid'])
-        querytochange=query.objects.get(id=queryid)
-        querytochange.query_status=True
-        user_id=querytochange.user_details_id
-        querytochange.save()
-        emailname=user.getusername(user.objects.get(id=user_id)).capitalize()
-        emailid=user.objects.get(id=user_id)
-        emailquerydate=querytochange.query_date.strftime('%d/%m/%Y')
-        emailtext="Dear "+emailname+","+"\nThis is to confirm that we have seen your query dated on "+emailquerydate+".\nWe assure you that the query will be resolved ASAP. \nRegards  "
-        emailsubject="Acknowledgement for your query"
-        send_mail(emailsubject, emailtext, 'querymanagementsystem@software', [emailid])
-        query_list=list(query.objects.filter(query_status=False).values())
-        for i in query_list:
-            i['user_details_id']=user.getusername(user.objects.get(id=i.get("user_details_id"))).capitalize()
-        return render(request,"admin_end/admin_end.html",{'query_list':query_list})
+        if 'acknowledgement' in request.POST:
+            queryid=(request.POST['queryid'])
+            querytochange=query.objects.get(id=queryid)
+            querytochange.query_status=True
+            user_id=querytochange.user_details_id
+            querytochange.save()
+            emailname=user.getusername(user.objects.get(id=user_id)).capitalize()
+            emailid=user.objects.get(id=user_id)
+            emailquerydate=querytochange.query_date.strftime('%d/%m/%Y')
+            emailtext="Dear "+emailname+","+"\nThis is to confirm that we have seen your query dated on "+emailquerydate+".\nWe assure you that the query will be resolved ASAP. \nRegards  "
+            emailsubject="Acknowledgement for your query"
+            send_mail(emailsubject, emailtext, 'querymanagementsystem@software', [emailid])
+            query_list=list(query.objects.filter(query_status=False).values())
+            for i in query_list:
+                i['user_details_id']=user.getusername(user.objects.get(id=i.get("user_details_id"))).capitalize()
+            return render(request,"admin_end/admin_end.html",{'query_list':query_list})
+        elif 'viewquery' in request.POST:
+            return render(request,"admin_end/admin_view_description.html")
 def logout_admin(request):
     logout(request)
     return redirect("/")
