ea52c058276052533520f6afffadd0b92dc17afc
vishwanath-29 <vishwanathnarayanan29@gmail.com>
2022-12-17 15:22:22 +0530

    Static Files issue

--- query_management_system/urls.py
+++ query_management_system/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('',include('user_end.urls')),
     path('',include('accounts.urls')),
     path('',include("admin_end.urls")),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
